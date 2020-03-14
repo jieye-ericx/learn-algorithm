120044807b20544280af13bb98c8eb107ea2ab75
radoapx <radoapx@gmail.com>
2020-03-15 01:31:56 +0800

    20200315

--- lanqiao/wangjieshiti/prev-5.cpp
+++ lanqiao/wangjieshiti/prev-5.cpp
@@ -1,3 +1,48 @@
+// 错误票据
+
+// 资源限制
+// 时间限制：1.0s   内存限制：256.0MB
+// 问题描述
+// 某涉密单位下发了某种票据，并要在年终全部收回。
+
+// 每张票据有唯一的ID号。全年所有票据的ID号是连续的，但ID的开始数码是随机选定的。
+
+// 因为工作人员疏忽，在录入ID号的时候发生了一处错误，造成了某个ID断号，另外一个ID重号。
+
+// 你的任务是通过编程，找出断号的ID和重号的ID。
+
+// 假设断号不可能发生在最大和最小号。
+
+// 输入格式
+// 要求程序首先输入一个整数N(N<100)表示后面数据行数。
+
+// 接着读入N行数据。
+
+// 每行数据长度不等，是用空格分开的若干个（不大于100个）正整数（不大于100000），请注意行内和行末可能有多余的空格，你的程序需要能处理这些空格。
+
+// 每个整数代表一个ID号。
+
+// 输出格式
+// 要求程序输出1行，含两个整数m n，用空格分隔。
+
+// 其中，m表示断号ID，n表示重号ID
+
+// 样例输入1
+// 2
+// 5 6 8 11 9
+// 10 12 9
+// 样例输出1
+// 7 9
+// 样例输入2
+// 6
+// 164 178 108 109 180 155 141 159 104 182 179 118 137 184 115 124 125 129 168 196
+// 172 189 127 107 112 192 103 131 133 169 158
+// 128 102 110 148 139 157 140 195 197
+// 185 152 135 106 123 173 122 136 174 191 145 116 151 143 175 120 161 134 162 190
+// 149 138 142 146 199 126 165 156 153 193 144 166 170 121 171 132 101 194 187 188
+// 113 130 176 154 177 120 117 150 114 183 186 181 100 163 160 167 147 198 111 119
+// 样例输出2
+// 105 120
 #include <cmath>
 #include <iostream>
 #include <cstring>
@@ -7,21 +52,59 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <sstream>
 using namespace std;
-int n,i,cnt,sta,t,ans1,ans2,flag;
-int vis[100002];
-int a[100000];
-int main(){
+// int a[100];
+int main()
+{
+	int n, cnt, t, ans2, flag;
+	int vis[100002], minN = 0xffffff, maxN = 0;
+	char s;
 	//ios::sync_with_stdio(false);
-	cin>>t;
-	cnt=0;
-	while(cin>>n){
-		a[cnt++]=n;
+	cin >> t;
+	cin.get();
+	// cnt = 0;
+
+	for (int i = 0; i < t; i++)
+	{
+		string str;
+		getline(cin, str);
+		istringstream aaa(str);
+		while (aaa >> n)
+		{
+			minN = min(n, minN);
+			maxN = max(n, maxN);
+			vis[n]++;
+			// a[cnt++] = n;
+			if (vis[n] == 2)
+				ans2 = n;
+		}
 	}
-	for(i=0;i<cnt;i++){
-		cout<<a[i]<<" ";
-	}
+	// for (int i = 0; i < cnt; i++)
+	// 	cout << a[i] << " ";
+	for (int i = minN; i < maxN; i++)
+		if (!vis[i])
+		{
+			cout << i << " ";
+			break;
+		}
+	cout << ans2 << "\n";
 	return 0;
 }
-
-
+/*
+#include <iostream>
+#include <sstream> //istringstream 必须包含这个头文件
+#include <string>
+using namespace std;
+int main()
+{
+	string name;
+	getline(cin, name);
+	istringstream iaas(name);
+	int s;
+	while (iaas >> s)
+	{
+		cout << s << endl;
+	}
+}
+*/
--- lanqiao/wangjieshiti/prev-6.cpp
+++ lanqiao/wangjieshiti/prev-6.cpp
@@ -1,3 +1,145 @@
+// #include <cmath>
+// #include <iostream>
+// #include <cstring>
+// #include <string>
+// #include <algorithm>
+// #include <cstdio>
+// #include <vector>
+// #include <set>
+// #include <map>
+// using namespace std;
+// string x, y;
+// int minDep = 0xffffff;
+// int a[1002], b[1002], vis[1002], lens, head = 0, rear;
+// bool chec()
+// {
+// 	for (int i = head; i <= rear; i++)
+// 	{
+// 		if (a[i] != b[i])
+// 			return false;
+// 	}
+// 	return true;
+// }
+
+// void gcd(int dep)
+// {
+// 	if (chec())
+// 	{
+// 		minDep = min(minDep, dep);
+// 		return;
+// 	}
+// 	if (dep > 2 * lens)
+// 		return;
+// 	for (int i = head; i <= rear; i++)
+// 	{
+// 		if (vis[i] == 0)
+// 		{
+// 			if (a[i] == b[i])
+// 			{
+// 				continue;
+// 			}
+// 			vis[i] = 1;
+// 			a[i] = (a[i] + 1) % 2;
+// 			a[i + 1] = (a[i + 1] + 1) % 2;
+// 			gcd(dep + 1);
+// 			a[i] = (a[i] + 1) % 2;
+// 			a[i + 1] = (a[i + 1] + 1) % 2;
+// 			vis[i] = 0;
+// 		}
+// 	}
+// }
+
+// int main()
+// {
+// 	//ios::sync_with_stdio(false);
+// 	cin >> x >> y;
+// 	lens = x.size();
+// 	rear = lens - 1;
+// 	for (int i = 0; i < lens; i++)
+// 	{
+// 		a[i] = x[i] == 'o' ? 0 : 1;
+// 		b[i] = y[i] == 'o' ? 0 : 1;
+// 	}
+// 	if (chec())
+// 	{
+// 		cout << "0\n";
+// 		return 0;
+// 	}
+// 	//下面剪枝，把首尾相同的给去掉了
+// 	int t = 0;
+// 	while (1)
+// 	{
+// 		if (a[t] == b[t])
+// 		{
+// 			head++;
+// 			t++;
+// 		}
+// 		else
+// 			break;
+// 	}
+// 	t = lens - 1;
+// 	while (1)
+// 	{
+// 		if (a[t] == b[t])
+// 		{
+// 			rear--;
+// 			t--;
+// 		}
+// 		else
+// 			break;
+// 	}
+
+// 	gcd(0);
+// 	cout << minDep << "\n";
+// 	return 0;
+// }
+
+// 历届试题 翻硬币
+
+// 资源限制
+// 时间限制：1.0s   内存限制：256.0MB
+// 问题描述
+// 小明正在玩一个“翻硬币”的游戏。
+
+// 桌上放着排成一排的若干硬币。我们用 * 表示正面，用 o 表示反面（是小写字母，不是零）。
+
+// 比如，可能情形是：**oo***oooo
+
+// 如果同时翻转左边的两个硬币，则变为：oooo***oooo
+
+// 现在小明的问题是：如果已知了初始状态和要达到的目标状态，每次只能同时翻转相邻的两个硬币,那么对特定的局面，最少要翻动多少次呢？
+
+// 我们约定：把翻动相邻的两个硬币叫做一步操作，那么要求：
+
+// 输入格式
+// 两行等长的字符串，分别表示初始状态和要达到的目标状态。每行的长度<1000
+
+// 输出格式
+// 一个整数，表示最小操作步数。
+
+// 样例输入1
+// **********
+// o****o****
+// 样例输出1
+// 5
+// 样例输入2
+// *o**o***o***
+// *o***o**o***
+// 样例输出2
+// 1
+
+// ******************
+// ***oo**oo***oo****
+// 3
+
+// 下面为方法二，如果对应的第一个硬币状态相同，那么一定不会翻转第一个硬币，
+// 因此，当前面对应的硬币状态相同的时候，我们不必考虑，只需要考虑第一个不同的硬币的位置，
+// 将其翻转即可，当翻转相同后，同样不必考虑。 所以我们只需要从第一个硬币开始比较对应的状态，
+// 如果相同则跳过，如果不同则翻转本身和后面一个硬币，翻转后继续前一个步骤。
+/*
+不独不说这个方法妙，算是根据题目逻辑来分析写的，我自己写的方法一是dfs死算，尽力优化后还是有一个案例过不了，这个简单且合理
+
+*/
 #include <cmath>
 #include <iostream>
 #include <cstring>
@@ -7,18 +149,24 @@
 #include <vector>
 #include <set>
 #include <map>
-using namespace std;
-
-int gcd(int x,int y){
-	if(y==0) return x;
-	return gcd(y,x%y);
-}
-int x,y;
-int main(){
+																																					 using namespace std;
+string x, y;
+int minDep = 0;
+int  lens;
+int main()
+{
 	//ios::sync_with_stdio(false);
-	while(cin>>x>>y)
-	cout<<gcd(x,y);
+	cin >> x >> y;
+	lens = x.size();
+	for (int i = 0; i < lens - 1; i++)
+	{
+		if (x[i] != y[i])
+		{
+			minDep++;
+			x[i + 1] = x[i + 1] == 'o' ? '*' : 'o';
+		}
+	}
+
+	cout << minDep << "\n";
 	return 0;
 }
-
-
