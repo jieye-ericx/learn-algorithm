3744e8f00070cdbf7ee7b02154a56e0f34e2c912
radoapx <radoapx@gmail.com>
2020-06-14 00:18:09 +0800

    20200613 写了连续子数组 快排

---
+++ js/leetcode/连续子数组的最大和.js
@@ -0,0 +1,37 @@
+/**
+ * 输入一个整型数组，数组里有正数也有负数。数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+
+要求时间复杂度为O(n)。
+
+ 
+
+示例1:
+
+输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ 
+
+提示：
+
+1 <= arr.length <= 10^5
+-100 <= arr[i] <= 100
+
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let ans = new Array(nums.length).fill(0),
+    index = 1,
+    maxN = nums[0]
+  ans[0] = nums[0]
+  for (; index < nums.length; index++) {
+    ans[index] = Math.max(nums[index] + ans[index - 1], nums[index])
+    maxN = Math.max(maxN, ans[index])
+  }
+  return maxN
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
