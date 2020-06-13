89a84d411fbc0c1a1b5ba31c64a92ca548e0fe24
radoapx <radoapx@gmail.com>
2020-06-13 16:27:35 +0800

    20200613

---
+++ js/a.js
@@ -0,0 +1,4 @@
+function warnUser() {
+    console.log("This is my warning message");
+}
+warnUser()
---
+++ js/a.ts
@@ -0,0 +1,15 @@
+function Greeter(target: Function): void {
+  target.prototype.greet = function (): void {
+    console.log("Hello Semlinker!");
+  };
+}
+
+@Greeter
+class Greeting {
+  constructor() {
+    // 内部实现
+  }
+}
+
+let myGreeting = new Greeting();
+myGreeting.greet(); // console output: 'Hello Semlinker!';
