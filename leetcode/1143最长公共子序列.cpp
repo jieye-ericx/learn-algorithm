4aa8dd89c215662e7d76ab3848a11658e44e9853
“radoapx” <radoapx@gmail.com>
2021-03-17 14:31:19 +0800

    20210317 mac

--- leetcode/1143最长公共子序列.cpp
+++ leetcode/1143最长公共子序列.cpp
@@ -9,25 +9,25 @@
 using namespace std;
 int longestCommonSubsequence(string t1, string t2)
 {
-    int dp[t1.size() + 1][t2.size() + 1];
-    memset(dp, 0, sizeof(dp));
-    for (int i = 1; i < t1.size() + 1; i++)
+  int dp[t1.size() + 1][t2.size() + 1];
+  memset(dp, 0, sizeof(dp));
+  for (int i = 1; i < t1.size() + 1; i++)
+  {
+    for (int j = 1; j < t2.size() + 1; j++)
     {
-        for (int j = 1; j < t2.size() + 1; j++)
-        {
-            if (t1[i - 1] == t2[j - 1])
-            {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            }
-            else
-            {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+      if (t1[i - 1] == t2[j - 1])
+      {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      }
+      else
+      {
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
     }
-    return dp[t1.size()][t2.size()];
+  }
+  return dp[t1.size()][t2.size()];
 }
 int main()
 {
-    reverse return 0;
+  return 0;
 }
