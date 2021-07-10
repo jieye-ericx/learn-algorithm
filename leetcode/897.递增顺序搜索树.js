88d29c4e9c720672c1dd92a80fd055a94ecfc755
radoapx-dell <radoapx@gmail.com>
2021-07-10 20:20:31 +0800

    1

---
+++ leetcode/897.递增顺序搜索树.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=897 lang=javascript
+ *
+ * [897] 递增顺序搜索树
+ *
+ * https://leetcode-cn.com/problems/increasing-order-search-tree/description/
+ *
+ * algorithms
+ * Easy (72.53%)
+ * Likes:    187
+ * Dislikes: 0
+ * Total Accepted:    40.4K
+ * Total Submissions: 54.2K
+ * Testcase Example:  '[5,3,6,2,4,null,8,1,null,null,null,7,9]'
+ *
+ * 给你一棵二叉搜索树，请你 按中序遍历
+ * 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [5,1,7]
+ * 输出：[1,null,5,null,7]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数的取值范围是 [1, 100]
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function (root) {
+
+    let preNode, ans, midOrder = (node) => {
+        if (node.left) midOrder(node.left)
+        node.left = null
+        let r = node.right
+        if (preNode === undefined) {
+            preNode = node
+            ans = node
+        }
+        else {
+
+            preNode.right = node
+            preNode = node
+        }
+        if (r) midOrder(r)
+    }
+    midOrder(root)
+    return ans
+};
+// @lc code=end
+
